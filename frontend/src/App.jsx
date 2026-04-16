8bb100e8415a26b37e2ee934f3dcde7c189e029c
ACHI LALLENE CEDRIC <lallene@MacBook-Pro-de-ACHI.local>
2026-04-16 17:48:53 +0200

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import {
   Legend,
 } from "recharts";
 
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL;
 const socket = API_URL ? io(API_URL) : null;
 
 function App() {
